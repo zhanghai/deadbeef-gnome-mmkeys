8313d0e03ff4d485a3e55c210857e0a6c159cf8e
r <r@white.(none)>
2012-08-17 00:11:38 +0400

    little improvements in build/install scripts and README

--- build.sh
+++ build.sh
@@ -7,3 +7,10 @@ libs=$(pkg-config --libs dbus-glib-1 dbus-1 glib-2.0)
 options="-std=c99 -shared -O2"
 
 gcc $includes $libs $options -o gnome_mmkeys.so gnome_mmkeys_marshal.c gnome_mmkeys.c
+
+[ $? -eq 0 ] && echo 'build successful' || {
+  echo 'build failed'
+  echo 'did you install prerequisites?'
+  echo '  sudo apt-get install libdbus-glib-1-dev deadbeef-plugins-dev'
+}
+
--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 
 cp gnome_mmkeys.so /usr/lib/deadbeef/
 chmod 644 /usr/lib/deadbeef/gnome_mmkeys.so
+
+[ $? -eq 0 ] && echo 'installation successful' || {
+  echo 'installation failed'
+  echo "usage: sudo sh $0"
+}
+
